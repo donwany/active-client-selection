d3456a009a707d0c434bff0369e96992d1300a03
hardtanh <fcdjin@hardtanh.com>
2023-11-04 21:52:25 -0500

    first commit

---
+++ src/simulations/sim_fedcifar100_resnet.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+python3 main.py \
+--method 'Random' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'AFL' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'Cluster1' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'Cluster2' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'Pow-d' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+-d 6 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'DivFL' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--subset_ratio 0.1 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
+
+python3 main.py \
+--method 'GradNorm' \
+--fed_algo 'FedAvg' \
+--dataset FedCIFAR100 \
+--model ResNet \
+-A 10 \
+-K 100 \
+--lr_local 0.1 \
+-B 20 \
+-R 200 \
+--data_dir '/home/ts2434/Desktop/active-client-selection/src/datasets/FederatedEMNIST/'
